6468806233299677341f69182cc3acefcf89f0e1
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2021-12-08 00:35:49 +0330

    use list instead of map in day6 solution

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,33 +1,26 @@
 module Day6 where
 
 import           Data.List.Split ( splitOn )
-import qualified Data.Map.Strict as M
 
-type Tally = M.Map Int Int
+type Tally = [Int]
 
 initTally :: [Int] -> Tally
-initTally = foldr (\n t -> M.insertWith (+) n 1 t) initialMap
-  where initialMap = M.fromList $ zip [0..8] (repeat 0)
+initTally xs = map (\x -> length $ filter (== x) xs) [0..8]
 
 step :: Tally -> Tally
-step t =
-  let (zs : lst) = M.elems t
-      -- shift tally to left
-      t' = M.fromList (zip [0..8] lst)
-      -- append new eights
-      t'' = M.insert 8 zs t'
-      -- add new sixes
-  in M.adjust (+ zs) 6 t''
+step [zero, one, two,   three, four, five,       six,   seven, eight]
+   = [one,  two, three, four,  five, six, seven + zero, eight, zero]
+step _ = error "not possible"
 
 solution :: Int -> Tally -> Int
-solution days tally = sum $ M.elems $ iterate step tally !! days
+solution days tally = sum $ iterate step tally !! days
 
 parseTimers :: String -> [Int]
 parseTimers = map read . splitOn ","
 
 main :: IO ()
 main = do
-  timers <- parseTimers <$> readFile "inputs/input6"
+  timers <- parseTimers <$> readFile "inputs/in"
   let tally = initTally timers
   print $ solution 80 tally
   print $ solution 256 tally
